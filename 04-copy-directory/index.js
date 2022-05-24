7ce7c51eb4eedd0a707ea57a1500ecaf39552569
Tema_sevastopol <temasebastopol@yandex.ru>
2022-05-24 20:46:27 +0300

    feat: solution #4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,18 @@
+const fs = require('fs');
+const path = require ('path');
 
+fs.mkdir(__dirname+'/files-copy',{recursive : true}, err =>{
+  if(err) throw err;  
+});
+const newPath = path.join(__dirname, 'files-copy');
+const oldPath = path.join(__dirname, 'files');
+console.log(newPath, oldPath);
+fs.readdir(__dirname+'/files', (err, data)=>{
+  if(err) throw err;
+  data.forEach(file =>{    
+    fs.copyFile(oldPath+'\\'+file, newPath+'\\'+file, err=>{
+      if(err) throw err;
+      console.log('Файл скопирован');
+    });
+  });
+});
