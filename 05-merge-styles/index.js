1e180fc2f661eb9e6cf58c2d1302ec0fae33e533
Tema_sevastopol <temasebastopol@yandex.ru>
2022-11-07 23:20:40 +0300

    fix: add then/catch

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,7 +4,7 @@ let fileItem ='';
 
 let bundleCss = fs.createWriteStream(path.join(__dirname, 'project-dist','bundle.css'));
 
-const readDir = (path) =>{
+const readDir = async (path) =>{
   return new Promise((resolve,reject) => fs.readdir(path, (err, data) => {  
     if(err){
       return reject(err.message);
@@ -14,8 +14,9 @@ const readDir = (path) =>{
 };
 
 readDir(path.join(__dirname, 'styles'))
-  .then((data)=>{    
-    data.forEach(file=>{      
+  .then((data)=>{     
+    data.forEach(file=>{   
+      console.log(file);
       if(path.extname(file).slice(1) === 'css'){      
         let stream = fs.createReadStream(path.join(__dirname, 'styles', file), 'utf-8');
         stream.on('data', chunk => fileItem += chunk); 
@@ -27,8 +28,5 @@ readDir(path.join(__dirname, 'styles'))
         stream.on('error', error => console.log('error ', error.message));                
       }   
     }); 
-  });
-
-    
-
-
+  })
+  .catch(err=>console.log(err));
