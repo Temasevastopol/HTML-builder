52eb6672a2e6aad9b554fcae868962b7dbebb66e
Tema_sevastopol <temasebastopol@yandex.ru>
2022-11-09 22:29:41 +0300

    fix: add function delDir, used rimraf

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,19 +1,33 @@
 const fs = require('fs');
-const path = require ('path');
+const path = require('path');
+const rimraf = require('rimraf');
 
-
-fs.mkdir(__dirname+'/files-copy',{recursive : true}, err =>{
-  if(err) throw err;  
-});
 const copyPath = path.join(__dirname, 'files-copy');
 const oldPath = path.join(__dirname, 'files');
 
-fs.readdir(oldPath, (err, data)=>{
-  if(err) throw err;
-  data.forEach(file =>{    
-    fs.copyFile(oldPath+'\\'+file, copyPath+'\\'+file, err=>{
-      if(err) throw err;
-      console.log('File was copied');
+console.log(copyPath);
+
+rimraf(copyPath, ()=>{
+  console.log('Directoy files-copy was deleted');    
+  makeDir();     
+});
+
+function makeDir(){
+  fs.mkdir(__dirname+'/files-copy',{recursive : true}, err =>{
+    if(err) throw err;  
+  }); 
+  copyFiles();  
+}
+
+function copyFiles(){
+  fs.readdir(oldPath, (err, data)=>{
+    if(err) throw err;
+    console.log(data);
+    data.forEach(file =>{    
+      fs.copyFile(oldPath+'\\'+file, copyPath+'\\'+file, err=>{
+        if(err) throw err;
+        console.log('File was copied');
+      });
     });
   });
-});
+}
